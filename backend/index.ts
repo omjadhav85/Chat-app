13433464427631b4e31233f526303d58c8561efb
Omkar Jadhav <48726860+omjadhav85@users.noreply.github.com>
2025-02-02 16:55:01 +0530

    [fix] added deployed url for socket endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -48,7 +48,7 @@ const server = app.listen(PORT, () =>
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-e4z2.onrender.com",
     // credentials: true,
   },
 });
