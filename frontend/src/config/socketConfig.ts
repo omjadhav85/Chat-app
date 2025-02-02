15a9c556c51f95887342c43b1cdea489451ba969
Omkar Jadhav <48726860+omjadhav85@users.noreply.github.com>
2025-02-02 17:07:24 +0530

    [deployment] use env variable instead of hardcoded endpoint for socket

--- frontend/src/config/socketConfig.ts
+++ frontend/src/config/socketConfig.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // const ENDPOINT = "http://localhost:5000";
-const ENDPOINT = "https://chat-app-e4z2.onrender.com";
+const ENDPOINT = import.meta.env.VITE_API_URL;
 
 export const socket = io(ENDPOINT, {
   autoConnect: false,
